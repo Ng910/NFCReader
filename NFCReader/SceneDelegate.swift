00cad50883ec3b63c54bbf39d875bbcd9ac27534
Ngoktkt <ngokt910@gmail.com>
2025-11-03 01:01:41 +0900

    セキュリティモードの実装

--- NFCReader/SceneDelegate.swift
+++ NFCReader/SceneDelegate.swift
@@ -12,7 +12,7 @@ import ComposableArchitecture
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    var splashOverlay: UIView?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
@@ -42,11 +42,24 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        splashOverlay?.removeFromSuperview()
+        splashOverlay = nil
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
+        guard let window = window else { return }
+            
+        if splashOverlay == nil {
+            let overlay = SplashViewController(store: Store(initialState: Splash.State(), reducer: Splash.init))
+            overlay.view.frame = window.bounds
+            splashOverlay = overlay.view
+        }
+        
+        if let overlay = splashOverlay {
+            window.addSubview(overlay)
+        }
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
