d4cff06aade1dca0248b448b787ef5f1a25a63cf
Ngoktkt <ngokt910@gmail.com>
2025-11-02 16:47:23 +0900

    Init

---
+++ NFCReader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NFCReader
+//
+//  Created by 長岡巧敏 on 2025/11/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
