324bc097068fd1b077d97b4a5e8e68386f27a9b4
Ngoktkt <ngokt910@gmail.com>
2025-11-03 15:20:07 +0900

    文言変更

--- NFCReader/Client/NFC/NFCClientLiveKey.swift
+++ NFCReader/Client/NFC/NFCClientLiveKey.swift
@@ -13,7 +13,7 @@ extension NFCClient: DependencyKey {
     static let liveValue = Self(
         NFCSessionStart: { delegate in
             let session = NFCTagReaderSession(pollingOption: [.iso18092], delegate: delegate)
-            session?.alertMessage = "カードを平らな面に置き、カードの下半分を隠すように iPhone をその上に置きます。"
+            session?.alertMessage = "デバイスにカードをかざしてください"
             session?.begin()
         },
         NFCSessionReaded: { session, tags in
