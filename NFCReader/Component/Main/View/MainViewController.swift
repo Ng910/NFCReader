b235aae09e1b31666b616c1e0fa11529ac0dd45d
Ngoktkt <ngokt910@gmail.com>
2025-11-23 00:07:39 +0900

    画面フォルダ構成の修正

---
+++ NFCReader/Component/Main/View/MainViewController.swift
@@ -0,0 +1,163 @@
+//
+//  MainViewController.swift
+//  NFCReader
+//
+//  Created by 長岡巧敏 on 2025/11/02.
+//
+
+import UIKit
+import ComposableArchitecture
+import Combine
+import Anchorage
+import CombineCocoa
+import CoreNFC
+
+class MainViewController: UIViewController, NFCTagReaderSessionDelegate {
+
+    // MARK: - TCA
+    private let store: StoreOf<Main>
+    private let viewStore: ViewStoreOf<Main>
+    private var cancellables: Set<AnyCancellable> = []
+    private let tableView = UITableView()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "ICカードを読み取ってください"
+        label.font = .systemFont(ofSize: 24, weight: .bold)
+        label.numberOfLines = 0
+        label.textAlignment = .left
+        label.textColor = .black
+        return label
+    }()
+    
+    let guideImage: UIImageView = {
+        let image = UIImageView()
+        image.contentMode = .scaleAspectFit
+        image.image = UIImage(named: "NFCReader")
+        return image
+    }()
+    
+    let readButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("読み取る", for: .normal)
+        button.layer.cornerRadius = 8
+        button.backgroundColor = UIColor.buttonColor
+        return button
+    }()
+    
+    init(store: StoreOf<Main>) {
+        self.store = store
+        self.viewStore = ViewStore(store, observe: { $0 })
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        setupLaout()
+        setupTableView()
+        setupBinding()
+        setupEvent()
+    }
+    
+    private func setupLaout() {
+        view.addSubview(titleLabel)
+        view.backgroundColor = .white
+        
+        titleLabel.topAnchor == view.safeAreaLayoutGuide.topAnchor + 32
+        titleLabel.leftAnchor == view.leftAnchor + 24
+        titleLabel.rightAnchor == view.rightAnchor - 24
+        
+        view.addSubview(guideImage)
+        guideImage.topAnchor == titleLabel.bottomAnchor + 32
+        guideImage.leftAnchor == view.leftAnchor + 24
+        guideImage.rightAnchor == view.rightAnchor - 24
+        
+        view.addSubview(readButton)
+        readButton.topAnchor >= guideImage.bottomAnchor + 32
+        readButton.leftAnchor == view.leftAnchor + 24
+        readButton.rightAnchor == view.rightAnchor - 24
+        readButton.bottomAnchor == view.safeAreaLayoutGuide.bottomAnchor - 20
+        readButton.heightAnchor == 52
+        
+        view.addSubview(tableView)
+        tableView.topAnchor == titleLabel.bottomAnchor + 32
+        tableView.leftAnchor == view.leftAnchor + 0
+        tableView.rightAnchor == view.rightAnchor + 0
+        tableView.bottomAnchor == readButton.topAnchor - 20
+    }
+
+    private func setupBinding() {
+        readButton
+            .tapPublisher
+            .sink { [weak self] _ in
+                guard let self else { return }
+                self.viewStore.send(.view(.readButtonTapped(self)))
+            }
+            .store(in: &cancellables)
+    }
+    
+    private func setupEvent() {
+        viewStore.publisher
+            .map(\.felica)
+            .removeDuplicates()
+            .sink { [weak self] _ in
+                self?.tableView.reloadData()
+            }
+            .store(in: &cancellables)
+    }
+    
+    // MARK: - TableView
+    private func setupTableView() {
+        tableView.register(FelicaDetailCell.self, forCellReuseIdentifier: "FelicaDetailCell")
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 60
+        tableView.tableFooterView = UIView()
+        tableView.backgroundColor = .white
+    }
+
+    func tagReaderSessionDidBecomeActive(_ session: NFCTagReaderSession) {
+        DispatchQueue.main.async {
+            if let sceneDelegate = self.view.window?.windowScene?.delegate as? SceneDelegate {
+                sceneDelegate.hiddnSplashOverlay()
+            }
+        }
+    }
+    
+    func tagReaderSession(_ session: NFCTagReaderSession, didDetect tags: [NFCTag]) {
+        DispatchQueue.main.async {
+            self.viewStore.send(.view(.readSuccess(session, tags)))
+        }
+    }
+    
+    func tagReaderSession(_ session: NFCTagReaderSession, didInvalidateWithError error: Error) {
+        let readerError = error as! NFCReaderError
+        print(readerError.code, readerError.localizedDescription)
+    }
+}
+
+
+// MARK: - UITableViewDataSource / Delegate
+extension MainViewController: UITableViewDataSource, UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewStore.felica?.historys.count ?? 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "FelicaDetailCell", for: indexPath) as? FelicaDetailCell,
+              let history = viewStore.felica?.historys[indexPath.row]
+        else { return UITableViewCell() }
+
+        cell.date = history.useDate
+        cell.balance = history.balance
+        return cell
+    }
+}
